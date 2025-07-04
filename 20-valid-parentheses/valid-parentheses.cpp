377831a43f83ee667353bf53ca4bcb6e62df4b46
Yuvashree <yuvashree.bhuviee@gmail.com>
2025-07-04 14:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (61.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) { //{[()]}
+        stack<char> st;
+        for (char ch : s) { // 01234
+            if (ch == '(' || ch == '{' || ch=='[') {
+                st.push(ch);
+            } else {
+                if (st.empty())
+                    return false;
+                if ((ch == ')' && st.top() != '(') ||
+                    (ch == '}' && st.top() != '{') ||
+                    (ch == ']' && st.top() != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        } // true or false
+        return st.empty();
+    }};
