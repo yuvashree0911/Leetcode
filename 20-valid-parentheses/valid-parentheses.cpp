299d2738ea9295f3e098bb4c6bbca60a437dbb90
Yuvashree <yuvashree.bhuviee@gmail.com>
2025-07-14 17:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,20 +1,15 @@
-class Solution {
+class Solution{
 public:
-    bool isValid(string s) { //{[()]}
+    bool isValid(string s) {//{[()]}
         stack<char> st;
-        for (char ch : s) { // 01234
-            if (ch == '(' || ch == '{' || ch=='[') {
+        for(char ch:s){//01234
+            if(ch=='(' || ch=='{' || ch=='['){
                 st.push(ch);
-            } else {
-                if (st.empty())
-                    return false;
-                if ((ch == ')' && st.top() != '(') ||
-                    (ch == '}' && st.top() != '{') ||
-                    (ch == ']' && st.top() != '[')) {
-                    return false;
-                }
-                st.pop();
-            }
-        } // true or false
-        return st.empty();
-    }};
+           }else{
+            if(st.empty()) return false;
+            if((ch==')'&&st.top()!='(')||(ch=='}'&&st.top()!='{')||(ch==']'&&st.top()!='[')){
+                return false; }st.pop();
+           }
+       }//true or false
+       return st.empty(); 
+ } };
